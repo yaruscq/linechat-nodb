e943e77aa02a22bcc4f692adc4f3d7e541ddb5fc
yaruscq <cq1.chou@gmail.com>
2024-10-27 00:31:16 +0800

    modify wsgi again

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ eventlet.monkey_patch()
 from app import socketio, app
 
 
-with app.app_context():
-    socketio.run(app)
-
-# if __name__ == "__main__":
+# with app.app_context():
 #     socketio.run(app)
+
+if __name__ == "__main__":
+    socketio.run(app)
 
